0186a02c19f5d2436919b9892f4af6e407821299
Ivan Shafran <vanobox07@fivt.in.ru>
2016-04-20 02:07:34 +0300

    added log

--- android/app/src/main/java/technotrack/musicadviser/player/PlayerActivity.java
+++ android/app/src/main/java/technotrack/musicadviser/player/PlayerActivity.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import technotrack.musicadviser.R;
 
 public class PlayerActivity extends AppCompatActivity implements IPlayerView {
+    private static final String TAG = "PlayerActivity";
+
     private RemoteControlReceiver mRemoteControlReceiver;
     private ComponentName mComponentName;
     private PlayerPresenter mPlayerPresenter;
@@ -17,6 +20,9 @@ public class PlayerActivity extends AppCompatActivity implements IPlayerView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate");
+
         setContentView(R.layout.activity_player);
 
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
@@ -30,6 +36,9 @@ public class PlayerActivity extends AppCompatActivity implements IPlayerView {
     @Override
     protected void onStart() {
         super.onStart();
+
+        Log.d(TAG, "onStart");
+
         mComponentName = new ComponentName(getPackageName(),
                 RemoteControlReceiver.class.getName());
         mAudioManager.registerMediaButtonEventReceiver(mComponentName);
@@ -42,6 +51,8 @@ public class PlayerActivity extends AppCompatActivity implements IPlayerView {
     protected void onResume() {
         super.onResume();
 
+        Log.d(TAG, "onResume");
+
         mPlayerPresenter.onResume();
     }
 
@@ -49,12 +60,17 @@ public class PlayerActivity extends AppCompatActivity implements IPlayerView {
     protected void onPause() {
         super.onPause();
 
+        Log.d(TAG, "onPause");
+
         mPlayerPresenter.onPause();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        Log.d(TAG, "onStop");
+
         mAudioManager.unregisterMediaButtonEventReceiver(mComponentName);
 
         mPlayerPresenter.onStop();
